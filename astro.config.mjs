83fef233d3257bb8779fdd389ada95809e4a9b60
shichen437 <shichen437@126.com>
2026-01-03 16:43:05 +0800

    build: domain update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gowlive.github.io",
+  site: "https://shichen437.github.io",
   base: "/",
   output: "static",
   integrations: [
